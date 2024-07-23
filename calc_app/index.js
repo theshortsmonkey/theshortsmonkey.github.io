6aa6062e59832139a53ce78edd37ec5e28b78b73
theshortsmonkey <j.horsley72@gmail.com>
2024-07-23 20:45:58 +0100

    ensured result and calc display updated at the correct point

--- calc_app/index.js
+++ calc_app/index.js
@@ -1,60 +1,61 @@
-import Calculator from './utils/calc.js'
+import Calculator from "./utils/calc.js";
 
 const numbers = [...document.getElementsByClassName("number")];
 const symbols = [...document.getElementsByClassName("symbol")];
-const equalsSqaure = document.getElementById('s-equals')
-const clearSqaure = document.getElementById('s-clear')
-const clearAllSqaure = document.getElementById('s-clear-all')
-const historySquare = document.getElementById('s-history')
+const equalsSqaure = document.getElementById("s-equals");
+const clearSqaure = document.getElementById("s-clear");
+const clearAllSqaure = document.getElementById("s-clear-all");
+const historySquare = document.getElementById("s-history");
 
-const calculationDisplay = document.getElementById('current-calculation')
-const resultDisplay = document.getElementById('current-result')
-let currentCalculation = ''
-let calculator = new Calculator;
+const calculationDisplay = document.getElementById("current-calculation");
+const resultDisplay = document.getElementById("current-result");
+let currentCalculation = "";
+let calculator = new Calculator();
 
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
-    currentCalculation+=number.textContent
-    calculationDisplay.textContent = `Calculation: ${currentCalculation}`
+    currentCalculation += number.textContent;
+    calculationDisplay.textContent = `Calculation: ${currentCalculation}`;
+    resultDisplay.textContent = `Result:`;
   });
 });
 
 symbols.forEach((symbol) => {
   symbol.addEventListener("click", () => {
-    currentCalculation+=` ${symbol.textContent} `
-    calculationDisplay.textContent = `Calculation: ${currentCalculation}`
+    currentCalculation += ` ${symbol.textContent} `;
+    calculationDisplay.textContent = `Calculation: ${currentCalculation}`;
+    resultDisplay.textContent = `Result:`;
   });
 });
 
-equalsSqaure.addEventListener("click",() => {
-  evaluateCalculation()
-})
+equalsSqaure.addEventListener("click", () => {
+  evaluateCalculation();
+});
 
 const evaluateCalculation = () => {
-  calculator.runCalculation(currentCalculation)
-  resultDisplay.textContent = `Result: ${calculator.output}`
-  currentCalculation = ''
-  calculationDisplay.textContent = `Calculation:`
-}
+  calculator.runCalculation(currentCalculation);
+  resultDisplay.textContent = `Result: ${calculator.output}`;
+  currentCalculation = "";
+};
 
-clearSqaure.addEventListener("click",() => {
-  calculatorClear()
-})
+clearSqaure.addEventListener("click", () => {
+  calculatorClear();
+});
 
 const calculatorClear = () => {
-  calculator.clear()
-  resultDisplay.textContent = `Result:`
-  currentCalculation = ''
-  calculationDisplay.textContent = `Calculation:`
-}
+  calculator.clear();
+  resultDisplay.textContent = `Result:`;
+  currentCalculation = "";
+  calculationDisplay.textContent = `Calculation:`;
+};
 
-clearAllSqaure.addEventListener("click",() => {
-  calculatorClearAll()
-})
+clearAllSqaure.addEventListener("click", () => {
+  calculatorClearAll();
+});
 
 const calculatorClearAll = () => {
-  calculator.clearAll()
-  resultDisplay.textContent = `Result:`
-  currentCalculation = ''
-  calculationDisplay.textContent = `Calculation:`  
-}
+  calculator.clearAll();
+  resultDisplay.textContent = `Result:`;
+  currentCalculation = "";
+  calculationDisplay.textContent = `Calculation:`;
+};
